3ea42da9c0bf68b6aaa114d5aba6eb67b441c750
Grimer123 <35968798+Grimer123@users.noreply.github.com>
2021-04-11 23:26:16 +0300

    Update setup_mn50.sh

--- setup_mn50.sh
+++ setup_mn50.sh
@@ -48,9 +48,9 @@ if [[ $DOSETUP =~ "y" ]] ; then
   ## COMPILE AND INSTALL
   mkdir -p ~/monkey_tmp
   cd ~/monkey_tmp
-  wget https://github.com/MONKEYPROJECT/MonkeyV2/releases/download/v2.2.0/monkey-2.2.0-x86_64-linux-gnu.tar.gz
-  tar -xvzf monkey-2.2.0-x86_64-linux-gnu.tar.gz
-  cd ./monkey-2.2.0/bin
+  wget https://github.com/decenomy/MonkeyV2/releases/download/v2.3.0/monkey-2.3.0-x86_64-linux-gnu.tar.gz
+  tar -xvzf monkey-2.3.0-x86_64-linux-gnu.tar.gz
+  cd ./monkey-2.3.0/bin
   sudo chmod 755 *
   sudo mv ./monkey* /usr/bin
   #read
@@ -76,11 +76,11 @@ NAME="monkey"
 
 MNCOUNT=""
 re='^[0-9]+$'
-# while ! [[ $MNCOUNT =~ $re ]] ; do
-   # echo ""
-   # echo "How many nodes do you want to create on this server?, followed by [ENTER]:"
-   # read MNCOUNT
-# done
+ while ! [[ $MNCOUNT =~ $re ]] ; do
+    echo ""
+    echo "How many nodes do you want to create on this server?, followed by [ENTER]:"
+    #read MNCOUNT
+ done
 
 MNCOUNT=50
 ALIAS=1
@@ -124,31 +124,28 @@ for i in `seq 1 1 $MNCOUNT`; do
   echo "daemon=1" >> ${NAME}.conf_TEMP
   echo "logtimestamps=1" >> ${NAME}.conf_TEMP
   echo "maxconnections=256" >> ${NAME}.conf_TEMP
-  #echo "masternode=1" >> ${NAME}.conf_TEMP
+  echo "masternode=1" >> ${NAME}.conf_TEMP
 
   echo "" >> ${NAME}.conf_TEMP
   echo "port=$PORT" >> ${NAME}.conf_TEMP
-  # echo "masternodeaddr=$IP" >> ${NAME}.conf_TEMP
-  # echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
+  echo "masternodeaddr=$IP" >> ${NAME}.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
 
-  #old monk 
-  #echo "" >> ${NAME}.conf_TEMP
-  #echo "addnode=140.72.47.201" >> ${NAME}.conf_TEMP
-  #echo "addnode=8.9.3.216" >> ${NAME}.conf_TEMP
-  #echo "addnode=63.209.32.131" >> ${NAME}.conf_TEMP
+  echo "" >> ${NAME}.conf_TEMP
+  echo "addnode=139.162.85.250:37233" >> ${NAME}.conf_TEMP
+  echo "addnode=140.82.18.99:37233" >> ${NAME}.conf_TEMP
+  echo "addnode=144.91.87.251:37233" >> ${NAME}.conf_TEMP
   
-  #echo "addnode=144.202.62.242" >> ${NAME}.conf_TEMP
-  #echo "addnode=149.28.118.89" >> ${NAME}.conf_TEMP
-  #echo "addnode=95.179.140.122" >> ${NAME}.conf_TEMP
-  #echo "addnode=95.179.137.202" >> ${NAME}.conf_TEMP
-  #echo "addnode=45.76.39.180" >> ${NAME}.conf_TEMP
-  #echo "addnode=95.179.140.231" >> ${NAME}.conf_TEMP
-  #echo "addnode=95.179.148.213" >> ${NAME}.conf_TEMP
+  echo "addnode=207.180.212.69:37233" >> ${NAME}.conf_TEMP
+  echo "addnode=173.212.238.71:37233" >> ${NAME}.conf_TEMP
+  echo "addnode=173.249.63.194:37233" >> ${NAME}.conf_TEMP
+  echo "addnode=78.159.150.119:37233" >> ${NAME}.conf_TEMP
+ 
 
   sudo ufw allow $PORT/tcp
   mv ${NAME}.conf_TEMP $CONF_DIR/${NAME}.conf
 
-  # sh ~/bin/${NAME}d_$ALIAS.sh
+  sh ~/bin/${NAME}d_$ALIAS.sh
   
   ALIAS=$[ALIAS + 1]
   PORT=$[PORT + 1]  
