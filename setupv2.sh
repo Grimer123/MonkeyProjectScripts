e9b0557e86f54bc2c8e5eabf2f80236c5fb34eca
Grimer123 <35968798+Grimer123@users.noreply.github.com>
2021-04-11 23:21:17 +0300

    Update setupv2.sh

--- setupv2.sh
+++ setupv2.sh
@@ -50,9 +50,9 @@ if [[ ${DOSETUP,,} =~ "y" ]] ; then
   ## COMPILE AND INSTALL
   mkdir -p ~/monkey_tmp
   cd ~/monkey_tmp
-  wget https://github.com/MONKEYPROJECT/MonkeyV2/releases/download/v2.2.0/monkey-2.2.0-x86_64-linux-gnu.tar.gz
-  tar -xvzf monkey-2.2.0-x86_64-linux-gnu.tar.gz
-  cd ./monkey-2.2.0/bin
+  wget https://github.com/decenomy/MonkeyV2/releases/download/v2.3.0/monkey-2.3.0-x86_64-linux-gnu.tar.gz
+  tar -xvzf monkey-2.3.0-x86_64-linux-gnu.tar.gz
+  cd ./monkey-2.3.0/bin
   sudo chmod 755 *
   sudo mv ./monkey* /usr/bin
   #read
